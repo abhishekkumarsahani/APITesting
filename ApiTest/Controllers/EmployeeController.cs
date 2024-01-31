5a72e49c1457b2413eddc3c2897367fa4aeebbb3
abhishekkumarsahani <126187842+abhishekkumarsahani@users.noreply.github.com>
2024-01-31 16:06:00 +0545

    Initial Commit

---
+++ ApiTest/Controllers/EmployeeController.cs
@@ -0,0 +1,147 @@
+﻿using ApiTest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public EmployeeController(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+        {
+            _configuration = configuration;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select EmployeeId, EmployeeName,Department, convert(varchar(10),DateofJoining,120) as DateofJoining,
+                            PhotoFileName from dbo.Employee
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Employee emp)
+        {
+            string query = @"
+                            insert into dbo.Employee (EmployeeName,Department,DateofJoining,PhotoFileName) values (@EmployeeName,@Department,@DateofJoining,@PhotoFileName)
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@EmployeeName", emp.EmployeeName);
+                    myCommand.Parameters.AddWithValue("@Department", emp.Department);
+                    myCommand.Parameters.AddWithValue("@DateofJoining", emp.DateofJoining);
+                    myCommand.Parameters.AddWithValue("@PhotoFileName", emp.PhotoFileName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+        [HttpPut]
+        public JsonResult Put(Employee emp)
+        {
+            string query = @"
+                            update dbo.Employee set EmployeeName = @EmployeeName,
+                            Department = @Department,
+                            DateofJoining =@DateofJoining,
+                            PhotoFileName = @PhotoFileName
+                            where EmployeeId = @EmployeeId
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@EmployeeId", emp.EmployeeId);
+                    myCommand.Parameters.AddWithValue("@EmployeeName", emp.EmployeeName);
+                    myCommand.Parameters.AddWithValue("@Department", emp.Department);
+                    myCommand.Parameters.AddWithValue("@DateofJoining", emp.DateofJoining);
+                    myCommand.Parameters.AddWithValue("@PhotoFileName", emp.PhotoFileName); myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            return new JsonResult("Update Successfully");
+        }
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                     delete from dbo.Employee where EmployeeId = @EmployeeId
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@EmployeeId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFiles()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _webHostEnvironment.ContentRootPath+"/Photos/" + filename;
+
+                using(var stream=new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+                return new JsonResult(filename);
+            }
+            catch (Exception)
+            {
+                return new JsonResult("anonymous.png");
+            }
+        }
+    }
+}
---
+++ apitestingui/src/App.js
@@ -0,0 +1,42 @@
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
+import { Home } from './Home';
+import { Department } from './Department';
+import { Employee } from './Employee';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App container'>
+        <h3 className='d-flex justify-content-center m-3'>
+          React JS Frontend
+        </h3>
+        <nav className='navbar navbar-expand-sm bg-light navbar-dark'>
+          <ul className='navbar-nav'>
+            <li className='nav-item- m-1'>
+              <NavLink className="btn btn-light btn-outline-primary" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className='nav-item- m-1'>
+              <NavLink className="btn btn-light btn-outline-primary" to="/department">
+                Department
+              </NavLink>
+            </li>
+            <li className='nav-item- m-1'>
+              <NavLink className="btn btn-light btn-outline-primary" to="/Employee">
+                Employee
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/department' element={<Department />} />
+          <Route path='/employee' element={<Employee />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
